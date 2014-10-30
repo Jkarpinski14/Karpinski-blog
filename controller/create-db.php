00d2c6b7b3ab99507651a4b575735c1865af9c37
Jonah Karpinski <Jkarpinski14@yahoo.com>
2014-10-30 08:45:23 -0700

    Finished video 14 - Creating video for blog posts

--- controller/create-db.php
+++ controller/create-db.php
@@ -3,6 +3,7 @@
 	/*links to database.php*/
 
 	$connection = new mysqli($host, $username, $password);
+	/*connects to arguments in database.php; has to be in same order on both pgaes*/
 
 	if($connection->connect_error){
 		die("Error: " . $connection->connect_error);
@@ -21,8 +22,14 @@
 		echo "Database already exists.";
 	}
 	/*the "if statement" checks if the database does not exist, and creates it if it doesn't.
-	If it does exist, then the else statement is run.*/
+	If it does exist, then the else statement is run.*/	
 	/*exclamation point means not*/
+
+	$query = $connection->query("CREATE TABLE posts ("
+		. "id int(11) NOT NULL AUTO_INCREMENT,"
+		. "title varchar(255) NOT NULL,"
+		. "post test NOT NULL,"
+		. "PRIMARY KEY (id)"); 
 
 	$connection->close();
 ?>
