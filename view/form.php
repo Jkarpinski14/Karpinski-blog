01f955a8d90452152e39d03316edd327f8207431
Jonah Karpinski <Jkarpinski14@yahoo.com>
2014-11-05 08:53:15 -0800

    Video 19 completed - Creating a blog post form

---
+++ view/form.php
@@ -0,0 +1,15 @@
+<h1>Create Blog Post</h1>
+
+<form>
+	<div>
+		<label for="title">Title: </label>
+		<input type="text" name="Title" />
+		<!--creates the header title and the box to enter data in the title-->
+	</div>
+
+	<div>
+		<label for="post">Post: </label>
+		<textarea name="post"></textarea>
+		<!--creates the header post and the box to enter data in the post box-->
+	</div>
+</form>
