c2d566bc2cc5b3e46067e5dc3d5ac126b3fa5383
Jonah Karpinski <Jkarpinski14@yahoo.com>
2014-11-06 09:14:02 -0800

    More CSS changes & image added

--- view/form.php
+++ view/form.php
@@ -6,13 +6,16 @@
 		<input type="text" name="Title" />
 		<!--creates the header title and the box to enter data in the title-->
 	</div>
-
+	<br>
 	<div>
 		<label for="post">Post: </label>
 		<textarea name="post"></textarea>
 		<!--creates the header post and the box to enter data in the post box-->
 	</div>
+	<br>
 	<div>
 		<button type="submit">Submit</button>
 	</div>
+	<br>
+	<img src="http://images.radcity.net/6626/4966353.jpg" alt="nascar" height="420" width="560">
 </form>
