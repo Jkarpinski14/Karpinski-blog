121afeb963e47e666494e7b08f83cb1f4579a938
Jonah Karpinski <Jkarpinski14@yahoo.com>
2014-11-07 08:14:53 -0800

    Video 21 completed - Creating a controller for blog posts

---
+++ controller/create-post.php
@@ -0,0 +1,8 @@
+<?php
+	$title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
+	/*validates input and checks for malicious activity & makes sure it's a string*/
+	$post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
+	/*checks to see that informations is recieved, and makes sure that each aspect is compatible to the string*/
+
+	echo "<p>Title: $title</p>";
+	echo "<p>Post: $post</p>";
