3c0e83ffad02eae1bb8f3e20426458e09f454cf3
Jonah Karpinski <Jkarpinski14@yahoo.com>
2014-11-21 08:49:17 -0800

    Video 35 completed - Using sessions to store data

--- model/config.php
+++ model/config.php
@@ -1,5 +1,6 @@
 <?php
 	require_once(__DIR__ . "/Database.php");
+	session_start();
 
 	$path = "/Karpinski-blog/";
 	/*Requires the path an its location*/
@@ -9,4 +10,9 @@
 	$password = "root";
 	$database = "blog_db";
 
-	$connection = new Database($host, $username, $password, $database);
+	if(!isset($_SESSION["connection"])) {
+		$connection = new Database($host, $username, $password, $database);
+		$_SESSION["connection"] = $connection;
+		/*try to access session variable, called connection, in the brackets*/
+	}
+	/*runs when the session variable is empty, or has not been set*/
