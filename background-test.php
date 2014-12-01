5668e73e04ff829b3043d6931199ffe121eab00b
Jonah Karpinski <Jkarpinski14@yahoo.com>
2014-12-01 10:43:26 -0800

    New files created; testing background animation and gradient

---
+++ background-test.php
@@ -0,0 +1,9 @@
+<html>
+	<head>
+		<title>Test Background</title>
+		<link rel="stylesheet" type="text/css" href="css/test.css">
+	</head>
+	<body>
+		<h1>"It's not the destination, it's the journey." <small>- Corny phrase #14</small></h1>
+	</body>
+</html>
