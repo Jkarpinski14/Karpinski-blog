c75c4c5d26b28181a9b7527cb0547737157c0e94
Jonah Karpinski <Jkarpinski14@yahoo.com>
2014-12-02 08:45:07 -0800

    Video 37 completed - Creating a Registration Form

---
+++ view/register-form.php
@@ -0,0 +1,19 @@
+<form>
+	<div>
+		<label for="email">Email: </label>
+		<input type="text" name="email" />
+	</div>
+	<div>
+		<label for="username">Username: </label>
+		<input type="text" name="username" />
+	</div>
+	<div>
+		<label for="password">Password: </label>
+		<input type="password" name="password" />
+		<!--password will show up as bullet points instead of displaying the actual text to the screen-->
+	</div>
+	<div>
+		<button type="submit">Submit</button>
+	</div>
+</form>
+<!--this page will allow only a specific user (me) to post things on the blog post forum-->
