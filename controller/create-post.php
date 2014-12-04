4515dd8e7ce4f50b20c436f87a65598359a915fe
Jonah Karpinski <Jkarpinski14@yahoo.com>
2014-12-04 08:49:11 -0800

    Video 41 completed - Changing the Appearance of our Date

--- controller/create-post.php
+++ controller/create-post.php
@@ -15,7 +15,7 @@
 
 	if($query){
 		echo "<p>Successfully inserted post: $title</p>";
-		echo "Posted on: " . $date->format("M/D/Y") . " at " . $time->format("g:i");
+		echo "Posted on: " . $date->format("D " . "M d, " . "Y") . " at " . $time->format("g:i");
 	}
 	else {
 		echo "<p>" . $_SESSION["connection"]->error . "</p>";
