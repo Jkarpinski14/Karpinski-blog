b2d46cd2277feff16cae5e15d816a01a607e278f
Jonah Karpinski <Jkarpinski14@yahoo.com>
2014-12-08 10:40:41 -0800

    Video 46 completed - Creating a Login Form and Controller

---
+++ view/login-form.php
@@ -0,0 +1,23 @@
+<?php
+	require_once(__DIR__ . "/../model/config.php");
+
+?>
+<!--wants to access path file in controller-->
+
+<h1>Login</h1>
+
+<form method="post" action="<?php echo $path . "controller/login-user.php"?>">
+	<div>
+		<label for="username">Username: </label>
+		<input type="text" name="username" />
+	</div>
+	<div>
+		<label for="password">Password: </label>
+		<input type="password" name="password" />
+		<!--password will show up as bullet points instead of displaying the actual text to the screen-->
+	</div>
+	<div>
+		<button type="submit">Submit</button>
+	</div>
+</form>
+
