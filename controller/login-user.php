20aae4910ee01fc348175f56310d7c4dc51c4086
Jonah Karpinski <Jkarpinski14@yahoo.com>
2014-12-09 09:11:52 -0800

    Video 48 completed - Checking the Passwords for Login

--- controller/login-user.php
+++ controller/login-user.php
@@ -3,3 +3,24 @@
 
 	$username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
 	$password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
+	/*username and password sent in by the form*/
+
+	$query = $_SESSION["connection"]->query("SELECT salt, password FROM users WHERE username = '$username'");
+	/*retrievs the salt and the password from the users table, and makes sure to only get it from the table with information attached to a designated username*/
+
+	if($query->num_rows == 1){
+		$row = $query->fetch_array();
+		/*gets the stored array of information and places it in the row variable*/
+		if($row["password"] === crypt ($password, $row["salt"])){
+			/*triple equal sign makes the password case-sensitive*/
+			echo "<p>Login Successful!</p>";
+		}
+		else{
+			echo "<p>Invalid username and/or password</p>";
+		}
+	}
+	else{
+		echo "<p>Invalid username and/or password</p>";
+		/*doesn't permait false users to know which paramater they are entering incorrectly*/
+	}
+	/*checks that information is stored in the query*/
